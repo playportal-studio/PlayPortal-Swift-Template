2d38603acd28e122cb236aa9286438dc59cb19de
Lincoln Fraley <lincoln@dynepic.com>
2018-10-31 11:16:44 -0400

    Initial Commit

---
+++ PlayPortal-Swift-Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayPortal-Swift-Template
+//
+//  Created by Lincoln Fraley on 10/31/18.
+//  Copyright © 2018 Lincoln Fraley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
