909ef13e0dc211250215fdb683102c86cb84b9a2
Lincoln Fraley <lincoln@dynepic.com>
2018-10-31 12:21:15 -0400

    added settings view controller

---
+++ PlayPortal-Swift-Template/SettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTableViewCell.swift
+//  PlayPortal-Swift-Template
+//
+//  Created by Lincoln Fraley on 10/31/18.
+//  Copyright © 2018 Lincoln Fraley. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PlayPortal-Swift-Template/SettingsTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsTableViewController.swift
+//  PlayPortal-Swift-Template
+//
+//  Created by Lincoln Fraley on 10/31/18.
+//  Copyright © 2018 Lincoln Fraley. All rights reserved.
+//
+
+import Foundation
