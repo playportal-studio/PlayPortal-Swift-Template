7f16cd7218f1a70a884e1d5c226d70d7c5f1dd3e
Lincoln Fraley <lincoln@dynepic.com>
2018-11-01 09:16:06 -0400

    changed copyright

--- PlayPortal-Swift-Template/InitialViewController.swift
+++ PlayPortal-Swift-Template/InitialViewController.swift
@@ -3,7 +3,7 @@
 //  PlayPortal-Swift-Template
 //
 //  Created by Lincoln Fraley on 10/31/18.
-//  Copyright © 2018 Lincoln Fraley. All rights reserved.
+//  Copyright © 2018 Dynepic, Inc. All rights reserved.
 //
 
 import Foundation
